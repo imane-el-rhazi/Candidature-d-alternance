a1a16e225e3079a9097bd7d0344267aee2c37b96
imane-el-rhazi <115189677+imane-el-rhazi@users.noreply.github.com>
2025-02-16 16:09:44 +0100

    Create authParams.js Go to file krikounoureddine krikounoureddine Update authParams.js

---
+++ modules/authParams.js Go to file krikounoureddine krikounoureddine Update authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'samuel.szoniecky@univ-paris8.fr',
+    apiOmk:'http://localhost/omeka-s/api/',
+    ident: 'Rm8f02rEHNRNGiDr6WNIJrv2S7SQwvKk',
+    key:'Lxehc76LIPkBpxXxJ7J1EWGrSxhs7gzJ',
+    keyGitHub:''
+}
