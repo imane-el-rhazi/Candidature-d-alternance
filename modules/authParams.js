d8cf23f25d040b830f5cc88ef33f5813e7b97228
imane-el-rhazi <115189677+imane-el-rhazi@users.noreply.github.com>
2025-02-16 16:10:50 +0100

    Delete modules/authParams.js

--- modules/authParams.js
+++
@@ -1,8 +0,0 @@
-export const pa = {
-    'navbar':d3.select('#navbarMain'),
-    mail:'samuel.szoniecky@univ-paris8.fr',
-    apiOmk:'http://localhost/omeka-s/api/',
-    ident: 'Rm8f02rEHNRNGiDr6WNIJrv2S7SQwvKk',
-    key:'Lxehc76LIPkBpxXxJ7J1EWGrSxhs7gzJ',
-    keyGitHub:''
-}
